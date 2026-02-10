e726c1d4a260ec5a2f8eba265f4624af7c18e0c6
omar faruk <omarf01851@gmail.com>
2026-02-10 20:27:20 +0600

    Fix CORS for Netlify frontend

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
+        "https://qr-system-by-faruk.netlify.app",
         "http://127.0.0.1:5500",
         "http://localhost:5500"
     ],
